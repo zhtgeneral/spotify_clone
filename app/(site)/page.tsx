d84c07d200cee5e6fd42d60d91d73f4b2d774eca
Zhong Tan <zhtgeneral@gmail.com>
2023-12-31 19:08:49 -0800

    added songs to front page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,13 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import PageContent from "./components/pageContent";
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
+
   return (
     <div className='bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto'>
       <Header>
@@ -19,7 +25,7 @@ export default function Home() {
           </h1>
         </div>
         <div>
-          List of songs
+          <PageContent songs={songs}/>
         </div>
       </div>
     </div>
