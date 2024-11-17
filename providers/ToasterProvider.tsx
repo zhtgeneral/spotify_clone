44a47de850e637511eb3cf138da79fe43e46bd9b
Zhong <zhtgeneral@gmail.com>
2024-11-16 16:06:52 -0800

    changed style

--- providers/ToasterProvider.tsx
+++ providers/ToasterProvider.tsx
@@ -6,6 +6,17 @@ const ToasterProvider = () => {
 	return (
 		<Toaster
 			toastOptions={{
+				success: {
+					iconTheme: {
+						primary: "var(--main)",
+						secondary: "black"
+					},
+					ariaProps: {
+						role: "alert",
+						"aria-live": "off"
+					}
+				},
+				duration: 2000,
 				style: {
 					background: "#333",
 					color: "#fff",
